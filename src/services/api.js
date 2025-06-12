c9b91a69df3479b30bf1af6803fbf316930a37ea
shubhamrl <shubhamlasankar10@gmail.com>
2025-06-12 19:19:39 +0530

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://tb-backend-1.onrender.com/api',
+  baseURL: 'https://tb-backend-1.onrender.com',
 });
 
 // Har request me JWT attach karo
