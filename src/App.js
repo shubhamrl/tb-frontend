d61db200b6f25b720fd031589d8ffd563a65bfa7
shubhamrl <shubhamlasankar10@gmail.com>
2025-06-22 10:49:20 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -12,32 +12,31 @@ function App() {
   const token = localStorage.getItem('token');
   return (
     <Router>
-     <Routes>
-      <Route path="/verify-otp" element={<OtpVerify />} />
-  <Route
-    path="/"
-    element={token ? <Navigate to="/dashboard" /> : <Navigate to="/signup" />}
-  />
-  <Route path="/signup" element={<Signup />} />
-  <Route path="/login" element={<Login />} />
-  <Route
-    path="/dashboard"
-    element={token ? <UserDashboard /> : <Navigate to="/login" />}
-  />
-  <Route
-    path="/admin"
-    element={token ? <AdminDashboard /> : <Navigate to="/login" />}
-  />
-  <Route
-    path="/game/tb"
-    element={token ? <TBGamePage /> : <Navigate to="/login" />}
-  />
-  <Route
-    path="/game/spin"
-    element={token ? <SpinGamePage /> : <Navigate to="/login" />}
-  />
-</Routes>
-
+      <Routes>
+        <Route path="/verify-otp" element={<OtpVerify />} />
+        <Route
+          path="/"
+          element={token ? <Navigate to="/dashboard" replace /> : <Navigate to="/signup" replace />}
+        />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/login" element={<Login />} />
+        <Route
+          path="/dashboard"
+          element={token ? <UserDashboard /> : <Navigate to="/login" replace />}
+        />
+        <Route
+          path="/admin"
+          element={token ? <AdminDashboard /> : <Navigate to="/login" replace />}
+        />
+        <Route
+          path="/game/tb"
+          element={token ? <TBGamePage /> : <Navigate to="/login" replace />}
+        />
+        <Route
+          path="/game/spin"
+          element={token ? <SpinGamePage /> : <Navigate to="/login" replace />}
+        />
+      </Routes>
     </Router>
   );
 }
