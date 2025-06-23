8d08affc2c76a587311185c87e08cbac888a33bc
shubhamrl <shubhamlasankar10@gmail.com>
2025-06-23 15:25:12 +0530

    Create ManageUserPage.js

---
+++ src/pages/ManageUserPage.js
@@ -0,0 +1,7 @@
+const ManageUserPage = () => (
+  <div style={{ padding: 32 }}>
+    <h2>Search / Manage User Page</h2>
+    <p>Yaha search user aur balance management ka code aayega.</p>
+  </div>
+);
+export default ManageUserPage;
