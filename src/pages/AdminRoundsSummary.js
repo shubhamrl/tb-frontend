9d0fc6f3ad887db890b4cb534584549a816101f9
shubhamrl <shubhamlasankar10@gmail.com>
2025-06-23 15:25:51 +0530

    Create AdminRoundsSummary.js

---
+++ src/pages/AdminRoundsSummary.js
@@ -0,0 +1,7 @@
+const AdminRoundsSummary = () => (
+  <div style={{ padding: 32 }}>
+    <h2>Today Rounds Summary Page</h2>
+    <p>Yaha summary table ka code aayega.</p>
+  </div>
+);
+export default AdminRoundsSummary;
