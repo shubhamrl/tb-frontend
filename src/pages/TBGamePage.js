85375268c7237dd030ac39ce9952b1dbb4bde459
shubhamrl <shubhamlasankar10@gmail.com>
2025-07-15 15:08:09 +0530

    Update TBGamePage.js

--- src/pages/TBGamePage.js
+++ src/pages/TBGamePage.js
@@ -17,10 +17,17 @@ const socket = io('https://tb-backend-1.onrender.com', {
 });
 
 const IMAGE_LIST = [
-  { name: 'umbrella', src: '/images/umbrella.png' }, { name: 'football', src: '/images/Football.png' },
-  { name: 'sun', src: '/images/sun.png' }, { name: 'diya', src: '/images/diya.png' }, { name: 'cow', src: '/images/cow.png' },
-  { name: 'bucket', src: '/images/Bucket.png' }, { name: 'kite', src: '/images/kite.png' }, { name: 'spinningTop', src: '/images/spinning_Top.png' },
-  { name: 'rose', src: '/images/rose.png' }, { name: 'butterfly', src: '/images/Butterfly.png' }, { name: 'pigeon', src: '/images/pigeon.png' },
+  { name: 'umbrella', src: '/images/umbrella.png' },
+  { name: 'football', src: '/images/Football.png' },
+  { name: 'sun', src: '/images/sun.png' },
+  { name: 'diya', src: '/images/diya.png' },
+  { name: 'cow', src: '/images/cow.png' },
+  { name: 'bucket', src: '/images/Bucket.png' },
+  { name: 'kite', src: '/images/kite.png' },
+  { name: 'spinningTop', src: '/images/spinning_Top.png' },
+  { name: 'rose', src: '/images/rose.png' },
+  { name: 'butterfly', src: '/images/Butterfly.png' },
+  { name: 'pigeon', src: '/images/pigeon.png' },
   { name: 'rabbit', src: '/images/rabbit.png' }
 ];
 
